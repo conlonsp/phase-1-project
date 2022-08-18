49d4439de82fe8f940e6204f95f3e5ee2d50e1c1
samuelconlon <conlonsp@yahoo.com>
2022-08-18 19:26:25 -0400

    add appendBreweries func to script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', event => {
   fetchBreweries()
-  
 })
 
 function fetchBreweries() {
@@ -9,4 +8,16 @@ function fetchBreweries() {
   .then(breweries => appendBreweries(breweries))
 }
 
-
+function appendBreweries(breweries) {
+  const ul = document.getElementById('brewery-list')
+  breweries.forEach(brewery => {
+    // console.log(brewery)
+    // console.log(brewery.name)
+    // console.log(brewery.brewery_type)
+    let li = document.createElement('li')
+    let p = document.createElement('p')
+    li.textContent = `Name: ${brewery.name}`
+    p.textContent = `Type: ${brewery.brewery_type}`
+    ul.append(li, p)
+  })
+}
