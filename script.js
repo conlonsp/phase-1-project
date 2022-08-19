0cf6d1eb4cc80fcc980dfe60cf22d43bdd8eea24
samuelconlon <conlonsp@yahoo.com>
2022-08-18 20:47:58 -0400

    created getCityAndState func and updated fetchBreweries

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', event => {
   fetchBreweries()
+  getCityAndState()
 })
 
-function fetchBreweries() {
-  fetch('https://api.openbrewerydb.org/breweries')
+function getCityAndState() {
+  const form = document.getElementById('city-state-form')
+  form.addEventListener('submit', event => {
+    event.preventDefault()
+    const cityInput = document.getElementById('city')
+    const stateInput = document.getElementById('state')
+    let city = cityInput.value.toLowerCase()
+    let state = stateInput.value.toLowerCase()
+    console.log('city: ', city, 'state: ', state)
+    fetchBreweries(city, state)
+  })
+}
+
+function fetchBreweries(city, state) {
+  fetch(`https://api.openbrewerydb.org/breweries?by_city=${city}&by_state=${state}&per_page=100`)
   .then(resp => resp.json())
   .then(breweries => appendBreweries(breweries))
 }
@@ -11,9 +25,6 @@ function fetchBreweries() {
 function appendBreweries(breweries) {
   const ul = document.getElementById('brewery-list')
   breweries.forEach(brewery => {
-    // console.log(brewery)
-    // console.log(brewery.name)
-    // console.log(brewery.brewery_type)
     let li = document.createElement('li')
     let p = document.createElement('p')
     li.textContent = `Name: ${brewery.name}`
